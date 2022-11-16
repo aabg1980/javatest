70b49a4d508890d1df999c5959078a147754768d
Andrés Barrera <aabg1980@gmail.com>
2022-11-16 08:42:34 -0500

    Update NumeroPar.java

--- JavaJenkins/src/main/java/org/example/numeropar/NumeroPar.java
+++ JavaJenkins/src/main/java/org/example/numeropar/NumeroPar.java
@@ -4,6 +4,8 @@ public class NumeroPar {
 
     public static boolean esNumeroPar(int numero)
     {
+        if(numero<=0)
+            throw new RuntimeException("No es un numero positivo " + numero);
         if(numero%2==0)
             return true;
         return false;
